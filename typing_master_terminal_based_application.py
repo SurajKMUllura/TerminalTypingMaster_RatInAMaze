5f2f2e0e8271496e11211c1a7c391a5b2b820064
Suraj K M Ullura <118556014+SurajKMUllura@users.noreply.github.com>
2023-11-08 21:34:44 +0530

    Update typing_master_terminal_based_application.py

--- typing_master_terminal_based_application.py
+++ typing_master_terminal_based_application.py
@@ -2,6 +2,10 @@ import json
 import random
 import time
 import keyboard
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def update_leaderboard(username, wpm, category):
     # Load existing leaderboard
@@ -35,16 +39,13 @@ def load_words_from_json():
         words_data = json.load(file)
         return words_data['words']
 
-def get_user_input(prompt):
-    return input(prompt)
-
 def load_categories_from_json():
     with open('_internal/word_categories.json', 'r') as file:
         return json.load(file)
 
 def main():
     print("Welcome to the Typing Test Game!")
-    username = get_user_input("Enter your username: ")
+    username = input("Enter your username: ")
 
     categories = load_categories_from_json()
 
@@ -56,57 +57,66 @@ def main():
         print("2. Show Leaderboard")
         print("3. Exit")
 
-        choice = get_user_input("Enter your choice (1/2/3): ")
-
+        choice = input("Enter your choice (1/2/3): ")
+        
+        clear_screen()
         if choice == '1':
             # Randomly select a category
-            category = random.choice(list(categories.keys()))
+            for category in categories:
+                print(category)
+        
+            selected_category = input("Enter the category you want to play: ")
+            
+            if selected_category in categories:
+                print(f"\nCategory: {selected_category}")
+                words = categories[selected_category]
+                random.shuffle(words)
 
-            words = categories[category]
-            random.shuffle(words)
+                start_time = time.time()
+                end_time = start_time + random.uniform(30, 60)  # Set the test duration to 45 seconds
 
-            print(f"\nCategory: {category}")
-
-            start_time = time.time()
-            end_time = start_time + random.uniform(30, 60)  # Set the test duration to 45 seconds
-
-            current_word_index = 0
-            correct_words = 0
-
-            print("\nGame Duration: 30-60 seconds.\nType the following word:")
-            while time.time() < end_time and current_word_index < len(words):
-                test_text = words[current_word_index]
-                print("\n")
-                print(test_text)
+                current_word_index = 0
+                correct_words = 0
+                totalentry=0
+                print("\nGame Duration: 30-60 seconds.\nType the following word:")
+                while time.time() < end_time and current_word_index < len(words):
+                    test_text = words[current_word_index]
+                    print("\n")
+                    print(test_text)
 
 
-                while True:
-                    if keyboard.is_pressed('ctrl+q'):
-                        print("\nExiting the Typing Test. Goodbye!")
-                        return
-                    
-                    user_input = input()
+                    while True:
+                        totalentry+=1
+                        if keyboard.is_pressed('ctrl+q'):
+                            print("\nExiting the Typing Test. Goodbye!")
+                            return
+                        
+                        user_input = input()
 
-                    if user_input == test_text:
-                        current_word_index += 1
-                        correct_words += 1
-                        break
+                        if user_input == test_text:
+                            current_word_index += 1
+                            correct_words += 1
+                            break
 
-                    try:
-                        user_input = keyboard.read_event().name
-                    except KeyboardInterrupt:
-                        break
+                        try:
+                            user_input = keyboard.read_event().name
+                        except KeyboardInterrupt:
+                            break
 
-            typing_duration = time.time() - start_time
-            wpm = (correct_words / typing_duration) * 60 if typing_duration > 0 else 0
+                typing_duration = time.time() - start_time
+                wpm = (correct_words / typing_duration) * 60 if typing_duration > 0 else 0
 
-            print("\nEnd of the test.")
-            print(f"Words Typed Correctly: {correct_words}")
-            print(f"Your Typing Speed: {wpm:.2f} WPM")
+                print("\nEnd of the test.")
+                print(f"Words Typed Correctly: {correct_words}")
+                print(f"Accuracy: {(correct_words/totalentry)*100:.2f}%")
+                print(f"Your Typing Speed: {wpm:.2f} WPM")
 
-            update_leaderboard(username, wpm, category)
+                update_leaderboard(username, wpm, category)
+            else:
+                print("Category Not Found")
 
         elif choice == '2':
+            clear_screen()
             show_leaderboard()
 
         elif choice == '3':
